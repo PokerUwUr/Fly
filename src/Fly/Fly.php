a5e2a69239854cbd041616dc1dc42de56bba622f
Pokker77 <86625331+PokerUwUr@users.noreply.github.com>
2021-10-28 13:36:27 -0500

    Add files via upload

---
+++ src/Fly/Fly.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Fly;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+
+class Fly extends PluginBase implements Listener {
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+        switch($command->getName()){
+        case "fly":
+
+        if(!isset($args[0])){
+            $sender->sendMessage("§cUse /fly on\off ");
+            return true;
+        }
+        if($args[0] == "on"){
+             $sender->setAllowFlight(true);
+             $sender->sendMessage("§aActivaste Esta Monda");
+             $sender->addTitle("§aUn Pericaso Pa Que Vuele");
+         }
+         if($args[0] == "off"){
+            $sender->setAllowFlight(false);
+            $sender->setFlying(false);
+            $sender->sendMessage("§cEsta Monda Se Apago");
+            $sender->addTitle("§cJodaa Mani Se Apago");
+        }
+    }
+    return true;
+}
+}
